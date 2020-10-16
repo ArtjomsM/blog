05a521955d9a66aa506e256adc2f9f17301b4f42
ArtjomsM <artjoms.minejevs@gmail.com>
2020-10-16 16:56:22 +0300

    blade layouts created

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -7,7 +7,7 @@
 <main class="container">
     <div class="row">
     <div class="col text-center text-uppercase py-5">
-            <h2>About</h2>
+            <h2>Contact</h2>
         </div>
     </div>
 </main>
